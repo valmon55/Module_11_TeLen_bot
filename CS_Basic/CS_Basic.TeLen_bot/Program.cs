a893b0cae42841a3039457de772b0fb04a3886db
valmon55 <fedor_ka@mail.ru>
2023-08-03 16:06:47 +0300

    start

---
+++ CS_Basic/CS_Basic.TeLen_bot/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CS_Basic.TeLen_bot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
